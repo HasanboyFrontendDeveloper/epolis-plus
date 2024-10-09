ee9dd09e8eb3746eefdfb75cb47f84c728b4f548
Hasanboy <hasanboyturdijonov@gmail.com>
2024-10-09 11:56:34 +0500

    first commit

---
+++ script/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // Auto play 
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
